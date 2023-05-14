ed2c20f12a0969346b8f13030412818b714be4d7
c5 <c5huracan@protonmail.ch>
2023-05-14 14:40:49 -0500

    added user input prompt to script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # langchain-question-answer-app
-Using OpenAI, FAISS, and LangChain PyPDF to make a basic question answering python script to query local Unity documentation in .pdf format.
+
+Using the LangChain PyPDF Directory Loader, the FAISS vectorstore, and OpenAI Embeddings to make a basic question answering python script to query multiple locally stored PDF files. In this case, Unity documentation but this could be a directory with any PDF files with which you would like to interact.
--- unity-gpt.py
+++ unity-gpt.py
@@ -1,4 +1,3 @@
-from langchain.llms import OpenAI
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -7,8 +6,6 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 loader = PyPDFDirectoryLoader("../unity-training-docs/")
 
 pages=loader.load_and_split()
-
-llm = OpenAI(temperature=0.4)
 
 faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
 user_input = input("Enter your Unity question: ")
