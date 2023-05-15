1c2460ee68712134759b202c826b67c5dada2c39
c5 <c5huracan@protonmail.ch>
2023-05-14 20:37:31 -0500

    added comment about where to change the path to point to your PDFs

--- unity-gpt.py
+++ unity-gpt.py
@@ -2,13 +2,13 @@ from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-
-loader = PyPDFDirectoryLoader("../unity-training-docs/")
+# modifiy this directory path to where your pdfs are located; must use relative pathing
+loader = PyPDFDirectoryLoader("../unity-training-docs/") 
 
 pages=loader.load_and_split()
 
 faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
-user_input = input("Enter your Unity question: ")
+user_input = input("Enter your question: ")
 docs = faiss_index.similarity_search(user_input, k=2)
 for doc in docs:
     print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
